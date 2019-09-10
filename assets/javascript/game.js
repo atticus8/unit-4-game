1b409ea50fb0e8c36f05db345f39961d299c20a4
atticus8 <atticus8@gmail.com>
2019-09-10 13:49:19 -0400

    working code

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+var score = 0;
+var newScore = 0;
+
+
+
+var startUp = function() {
+    objectiveRandom();
+    
+    console.log("Red value:" + red);
+    
+    console.log("Blue value:" + blue);
+    
+    console.log("Yellow value:" + yellow);
+    
+    console.log("Green value:" + green);
+    var guesses = 0;
+    var score = 0;
+}
+
+
+var objectiveRandom = function() {
+    var objective = parseInt(Math.floor(Math.random() * 101) + 19);
+    console.log("objective:" + objective);
+    document.querySelector("#objective").innerHTML = objective;
+}
+
+
+var red = parseInt((Math.floor(Math.random() * 11) + 1));
+
+
+
+var blue = parseInt((Math.floor(Math.random() * 11) + 1));
+
+
+
+var yellow = parseInt((Math.floor(Math.random() * 11) + 1));
+
+
+
+var green = parseInt((Math.floor(Math.random() * 11) + 1));
+
+
+
+var guessRed = function() {
+    let newScore = (score + red);
+    document.querySelector("#score").innerHTML = newScore;
+    let score = newScore;
+}
+
+var guessBlue = function() {
+    newScore = (score + blue);
+    document.querySelector("#score").innerHTML = newScore;
+    let score = newScore;
+}
+
+var guessYellow = function() {
+    newScore = (score + yellow);
+    document.querySelector("#score").innerHTML = newScore;
+    let score = newScore;
+}
+
+var guessGreen = function() {
+    newScore = (score + green);
+    document.querySelector("#score").innerHTML = newScore;
+    let score = newScore;
+}
+document.querySelector("#redButton").addEventListener("click", guessRed);
+
+if(score = objective){
+    wins++
+}
+
+if(score > objective){
+    losses++
+}
